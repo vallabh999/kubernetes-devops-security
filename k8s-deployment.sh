aa2630c3f47e2976b7d03116d9ecd9f7e146dc44
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-11 22:50:51 +0530

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #k8s-deployment.sh
+echo $KUBECONFIG
 
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 # kubectl -n default get deployment ${deploymentName} > /dev/null
