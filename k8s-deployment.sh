75505c4507cb36f68ee956f72ea1eefdd62528a0
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-11 23:01:15 +0530

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,10 +3,14 @@
 # k8s-deployment.sh
 
 # Print environment variables
+export KUBECONFIG=$KUBECONFIG
 echo "KUBECONFIG path: $KUBECONFIG"
 echo "Image name: $imageName"
 echo "Deployment name: $deploymentName"
 echo "Container name: $containerName"
+
+kubectl auth can-i get deployments --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
+kubectl auth can-i create services --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
 
 # Replace placeholder in YAML file
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
