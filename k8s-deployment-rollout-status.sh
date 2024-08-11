27a364baeba4b819853c5797959d353e6c3986db
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-11 22:33:10 +0530

    Update k8s-deployment-rollout-status.sh

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -3,6 +3,7 @@
 #k8s-deployment-rollout-status.sh
 
 sleep 60s
+echo $KUBECONFIG
 
 if [[ $(kubectl -n app rollout status deploy ${deploymentName} --timeout 5s --kubeconfig=$KUBECONFIG) != *"successfully rolled out"* ]]; 
 then     
