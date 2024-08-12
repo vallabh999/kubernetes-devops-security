4445553b9322d99d89561cf41cb1a5a05478892b
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-12 17:37:21 +0530

    Create k8s-deployment-rollout-status-prod.sh

---
+++ k8s-deployment-rollout-status-prod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#k8s-deployment-rollout-status.sh
+
+sleep 30s
+
+# export KUBECONFIG=$KUBECONFIG
+# echo "KUBECONFIG path: $KUBECONFIG"
+echo "Image name: $imageName"
+echo "Deployment name: $deploymentName"
+echo "Container name: $containerName"
+
+# kubectl auth can-i get deployments --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
+# kubectl auth can-i create services --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
+
+if [[ $(kubectl -n prod rollout status deploy ${deploymentName} --timeout 30s) != *"successfully rolled out"* ]]; 
+then     
+	echo "Deployment ${deploymentName} Rollout has Failed"
+    kubectl -n prod rollout undo deploy ${deploymentName}
+    exit 1;
+else
+	echo "Deployment ${deploymentName} Rollout is Success"
+fi
