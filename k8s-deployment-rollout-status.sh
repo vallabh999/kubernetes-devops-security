99c89a26d222da086767aa3ee8d5d37721d99bf3
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-12 10:52:19 +0530

    Update k8s-deployment-rollout-status.sh

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -4,19 +4,19 @@
 
 sleep 60s
 
-export KUBECONFIG=$KUBECONFIG
-echo "KUBECONFIG path: $KUBECONFIG"
+# export KUBECONFIG=$KUBECONFIG
+# echo "KUBECONFIG path: $KUBECONFIG"
 echo "Image name: $imageName"
 echo "Deployment name: $deploymentName"
 echo "Container name: $containerName"
 
-kubectl auth can-i get deployments --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
-kubectl auth can-i create services --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
+# kubectl auth can-i get deployments --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
+# kubectl auth can-i create services --as=system:serviceaccount:jenkins:jenkins-deployer --namespace=app
 
-if [[ $(kubectl -n app rollout status deploy ${deploymentName} --timeout 5s --kubeconfig=$KUBECONFIG) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n app rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n app rollout undo deploy ${deploymentName} --kubeconfig=$KUBECONFIG
+    kubectl -n app rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
