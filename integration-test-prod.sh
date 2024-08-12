0d8e7505442fbc7333facd4a08a63c96750d0ee7
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-12 17:36:01 +0530

    Create integration-test-prod.sh

---
+++ integration-test-prod.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#integration-test.sh
+
+sleep 5s
+applicationURL="http://3.110.107.159"
+# applicationURI="/"
+serviceName="devsecops-svc"
+
+PORT=$(kubectl -n prod get svc/$serviceName -o json | jq '.spec.ports[] | select(.nodePort != null) | .nodePort')
+
+echo $PORT
+echo $applicationURL:$PORT  #/$applicationURI
+
+if [[ ! -z "$PORT" ]];
+then
+
+    response=$(curl -s $applicationURL:$PORT$applicationURI)
+    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
+
+    if [[ "$response" == 100 ]];
+        then
+            echo "Increment Test Passed"
+        else
+            echo "Increment Test Failed"
+            # exit 1;
+    fi;
+
+    if [[ "$http_code" == 200 ]];
+        then
+            echo "HTTP Status Code Test Passed"
+        else
+            echo "HTTP Status code is not 200"
+            exit 1;
+    fi;
+
+else
+        echo "The Service does not have a NodePort"
+        exit 1;
+fi;
