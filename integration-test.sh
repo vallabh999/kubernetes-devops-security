0a04bbb5b8c8749685ec26d17c709ae6abf83d0f
Vallabh <30625352+vallabh999@users.noreply.github.com>
2024-08-12 11:54:00 +0530

    Update integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -4,7 +4,7 @@
 
 sleep 5s
 
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n app get $serviceName -o json | jq '.spec.ports[] | select(.nodePort != null) | .nodePort')
 
 echo $PORT
 echo $applicationURL:$PORT/$applicationURI
